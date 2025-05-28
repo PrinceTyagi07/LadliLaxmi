4cfee182c7f066730b84fcfcc49383fd0e833332
“Kunal <kunalkoushik44@gmail.com>
2025-05-28 12:31:41 +0530

    Added working payment in donation

--- src/components/Profile/main.jsx
+++ src/components/Profile/main.jsx
@@ -11,9 +11,6 @@ import { useLocation } from "react-router-dom";
 const Main = ({user}) => {
   return (
     <div className="flex mx-auto flex-col ">
-      {/* <div className="flex justify-center items-center text-2xl text-blue-600 mt-4">
-        <h1>Welcome to your Dashboard, {user.name}!</h1></div> */}
-      
       <Routes>
         <Route path="/dashboardOverview" element={<DashboardOverview user={user} />} />
         <Route path="/downline" element={<Dashboard user={user} matrixChildren={user.matrixChildren} />} />
@@ -21,8 +18,6 @@ const Main = ({user}) => {
         <Route path="/donation" element={<Donation user={user} matrixChildren={user.matrixChildren} />} />
         <Route path="/transactions" element={<TransactionHistory walletTransactions={user.walletTransactions} />} />
       </Routes>
-      {/* <DashboardOverview user={user} />
-      <Dashboard matrixChildren={user.matrixChildren} /> */}
 
     </div>
   );
