1cc43ed8821e9919d3804707b2895d3dd176ef0f
PrinceTyagi07 <princetyagi1901@gmail.com>
2025-06-06 21:27:31 +0530

    new

--- src/components/Profile/Withdraw.jsx
+++ src/components/Profile/Withdraw.jsx
@@ -144,7 +144,7 @@ const Withdraw = ({ user }) => {
               onChange={handleChange}
               required
               min={1}
-              className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500"
+              className="w-full no-spinner border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
